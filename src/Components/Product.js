cf7e9e4d82d5926bf2aeb10c40de3a10b143b2b5
Bharath Kumar <pbkbharath1707@gmail.com>
2020-09-09 15:12:56 +0530

    context api

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./Product.css";
 import StarIcon from "@material-ui/icons/Star";
+import { useStateValue } from "../StateProvider";
+
 function Product({ id, title, price, image, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const addToBasket = (e) => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        price: price,
+        image: image,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -21,7 +36,7 @@ function Product({ id, title, price, image, rating }) {
         </div>
       </div>
       <img alt="" src={image} />
-      <button>Add to Cart</button>
+      <button onClick={addToBasket}>Add to Cart</button>
     </div>
   );
 }
--- src/Components/Subtotal.js
+++ src/Components/Subtotal.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../StateProvider";
 function Subtotal() {
+  const [{ basket }] = useStateValue();
+  let summer = 0;
+  {
+    basket.map((item) => (summer = summer + item.price));
+  }
+
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket?.length} items): <strong>$ {summer} </strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" />
