8b8d3b51d1d00771065321560ada8e843d1c25fe
Bharath Kumar <pbkbharath1707@gmail.com>
2020-09-09 11:33:15 +0530

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
 function Header() {
   return (
     <div className="header">
@@ -10,7 +12,11 @@ function Header() {
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
       <div className="header__search">
-        <input className="header__searchInput" type="text" />
+        <input
+          className="header__searchInput"
+          type="text"
+          placeholder="Search..."
+        />
         <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
@@ -25,6 +31,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
---
+++ src/Components/Home.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          alt=""
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        />
+        <div className="home__row">
+          <Product
+            id="123454321"
+            title="The lean startup: How constant Innovation Creates Radically Successful Businesss Paperback"
+            rating={5}
+            price={19.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          />
+          <Product
+            id="123454321332"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            rating={4}
+            price={239.0}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="1234563562"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            rating={3}
+            price={98.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="123454321321211"
+            title="Amazon Echo (3rd generation0 | Start speaker with Alexa, Charcoal Fabric"
+            rating={5}
+            price={98.99}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="123454777332"
+            title="New Apple ipad Pro (12-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            rating={4}
+            price={598.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="1232355454321332"
+            title="Samsung LC49RSSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WOHQ 5120 x 14440"
+            rating={4}
+            price={1094.98}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
