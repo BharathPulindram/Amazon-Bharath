977c46c0d0a85bc95962814b11d524627cdf5001
Bharath Kumar <pbkbharath1707@gmail.com>
2020-09-21 13:29:55 +0530

    Updates

---
+++ precache-manifest.89c8a0f39abcd61ccaf768ca7418ae43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f0911a24df6c036d790e4dbbd4e030",
+    "url": "/Amazon-Bharath/index.html"
+  },
+  {
+    "revision": "7b12c4569d6718f47df6",
+    "url": "/Amazon-Bharath/static/css/main.2d59a205.chunk.css"
+  },
+  {
+    "revision": "899a2c94837946613cd3",
+    "url": "/Amazon-Bharath/static/js/2.5caae534.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Amazon-Bharath/static/js/2.5caae534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b12c4569d6718f47df6",
+    "url": "/Amazon-Bharath/static/js/main.03c7ba43.chunk.js"
+  },
+  {
+    "revision": "5ff3e7c77638c9202dcf",
+    "url": "/Amazon-Bharath/static/js/runtime-main.79953317.js"
+  }
+]);
